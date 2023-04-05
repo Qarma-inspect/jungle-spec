3c3e25e7d3db1f81ea94628a8cc5f06eea7f7105
Paul Fleischer <pf@qarmainspect.com>
2023-04-05 09:08:24 +0200

    Update .formatter.exs to export locals_without_parens

--- .formatter.exs
+++ .formatter.exs
@@ -1,15 +1,18 @@
+locals_without_parens =  [
+  additional_properties: 1,
+  additional_properties: 2,
+  open_api_object: 1,
+  open_api_object: 2,
+  open_api_object: 3,
+  open_api_type: 2,
+  open_api_type: 3,
+  property: 2,
+  property: 3
+]
+
 [
   line_length: 130,
   inputs: ["{mix,.formatter}.exs", "{config,lib,test,test_lib}/**/*.{ex,exs}"],
-  locals_without_parens: [
-    additional_properties: 1,
-    additional_properties: 2,
-    open_api_object: 1,
-    open_api_object: 2,
-    open_api_object: 3,
-    open_api_type: 2,
-    open_api_type: 3,
-    property: 2,
-    property: 3
-  ]
+  locals_without_parens: locals_without_parens,
+  export: [locals_without_parens: locals_without_parens]
 ]

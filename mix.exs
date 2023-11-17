9e8c0fcc936bae88cbe85b0a2ac0b0a2f09ee8cd
Paul Fleischer <pf@qarmainspect.com>
2023-11-17 09:14:06 +0100

    Add `ex_doc` and other minor changes to mix.exs

--- mix.exs
+++ mix.exs
@@ -8,7 +8,11 @@ defmodule JungleSpec.MixProject do
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      elixirc_paths: elixirc_paths(Mix.env())
+      elixirc_paths: elixirc_paths(Mix.env()),
+      package: package(),
+      source_url: source_url(),
+      name: "JungleSpec",
+      description: "OpenAPI Specification made easier."
     ]
   end
 
@@ -21,8 +25,20 @@ defmodule JungleSpec.MixProject do
     ]
   end
 
+  defp package do
+    [
+      licenses: "MIT",
+      links: %{
+        "GitHub" => source_url()
+      }
+    ]
+  end
+
+  defp source_url(), do: "https://github.com/Qarma-inspect/jungle-spec"
+
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:open_api_spex, "~> 3.12"}
     ]
   end
